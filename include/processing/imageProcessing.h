7ad91be63574a121a3db2e8bce5d78d8b48c5bb4
kruziikrel13 <dev@michaelpetersen.io>
2025-10-15 21:53:51 +1000

    chore: remove redundant function declarations

--- include/processing/imageProcessing.h
+++ include/processing/imageProcessing.h
@@ -8,12 +8,6 @@ namespace IMAGE {
 
     namespace PROCESS {
 
-        void reverseColor(const IMAGE::ImageRaster&);
-
-        void adjustBrightness(const IMAGE::ImageRaster&, const signed short);
-
-        void adjustContrast(const IMAGE::ImageRaster&, const unsigned char, const unsigned char);
-
         void convertRGB2GREY(const IMAGE::ImageRaster&);
 
         void converGREY2RGB(const IMAGE::ImageRaster&);
@@ -23,14 +17,6 @@ namespace IMAGE {
         void convertRGB2SEPIA(const IMAGE::ImageRaster&);
 
         int  rotateImage(IMAGE::ImageRaster&);
-
-        void zoomImage(IMAGE::ImageRaster&, unsigned int, unsigned int, unsigned int, unsigned int);
-
-        void scaleImage(IMAGE::ImageRaster&, double);
-
-        void scaleImage(IMAGE::ImageRaster&, unsigned int, unsigned int);
-
-        void blurImage(IMAGE::ImageRaster&, unsigned int);
 
     } // end of namespace PROCESS
 
