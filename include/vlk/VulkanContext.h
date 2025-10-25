45481efc63e49ca81bc49ecf67ad48e43cce59df
kruziikrel13 <dev@michaelpetersen.io>
2025-10-25 21:03:08 +1000

    formatting

--- include/vlk/VulkanContext.h
+++ include/vlk/VulkanContext.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <string>
 #include <vulkan/vulkan.hpp>
 #include <vulkan/vulkan_raii.hpp>
 
@@ -18,18 +17,18 @@ class CVulkanContext {
     // void                          createComputePipeline();
     // void                          createDescriptorPool();
 
-    vk::raii::Context             m_context;
-    vk::raii::Instance            m_instance            = nullptr;
-    vk::raii::PhysicalDevice      m_physicalDevice      = nullptr;
-    vk::raii::Device              m_device              = nullptr;
-    vk::raii::Queue               m_computeQueue        = nullptr;
-    vk::raii::CommandPool         m_commandPool         = nullptr; // Use this to create command buffers with which to operate
-    vk::raii::DescriptorSetLayout m_descriptorSetLayout = nullptr;
-    vk::raii::ShaderModule        m_computeShader       = nullptr;
-    vk::raii::PipelineLayout      m_pipelineLayout      = nullptr;
-    vk::raii::DescriptorPool      m_descriptorPool      = nullptr;
-    vk::raii::CommandBuffer       m_commandBuffer       = nullptr;
-    vk::raii::DescriptorSet       m_descriptorSet       = nullptr;
+    vk::raii::Context                   m_context;
+    vk::raii::Instance                  m_instance            = nullptr;
+    vk::raii::PhysicalDevice            m_physicalDevice      = nullptr;
+    vk::raii::Device                    m_device              = nullptr;
+    vk::raii::Queue                     m_computeQueue        = nullptr;
+    vk::raii::CommandPool               m_commandPool         = nullptr; // Use this to create command buffers with which to operate
+    vk::raii::DescriptorSetLayout       m_descriptorSetLayout = nullptr;
+    vk::raii::ShaderModule              m_computeShader       = nullptr;
+    vk::raii::PipelineLayout            m_pipelineLayout      = nullptr;
+    vk::raii::DescriptorPool            m_descriptorPool      = nullptr;
+    vk::raii::CommandBuffer             m_commandBuffer       = nullptr;
+    vk::raii::DescriptorSet             m_descriptorSet       = nullptr;
 
     vk::raii::Buffer              m_indexBuffer       = nullptr;
     vk::raii::DeviceMemory        m_indexBufferMemory = nullptr;
