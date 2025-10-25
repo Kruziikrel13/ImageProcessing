b13d2317290310e3c0d2af175d710c9da432afed
kruziikrel13 <dev@michaelpetersen.io>
2025-10-25 21:03:31 +1000

    feat: implement readFile function in shared lib

--- include/shared.h
+++ include/shared.h
@@ -1,2 +1,5 @@
 
 int getDirFileNames(const std::string dir, std::vector<std::string>& files);
+
+// Primarily for loading shaders, however does arbitrarily load files
+std::vector<char> readFile(const std::string&);
--- src/shared.cpp
+++ src/shared.cpp
@@ -1,8 +1,25 @@
 #include "shared.h"
+#include <fstream>
 
 int getDirFileNames(const std::string dir, std::vector<std::string>& files) {
     for (const auto& entry : std::filesystem::directory_iterator(dir))
         files.emplace_back(entry.path().filename().string());
 
     return 1;
+}
+
+std::vector<char> readFile(const std::string& filename) {
+    std::ifstream file(filename, std::ios::ate | std::ios::binary);
+
+    if (!file.is_open()) {
+        throw std::runtime_error("failed to open file: " + filename);
+    }
+
+    std::vector<char> buffer(file.tellg());
+
+    file.seekg(0, std::ios::beg);
+    file.read(buffer.data(), static_cast<std::streamsize>(buffer.size()));
+    file.close();
+
+    return buffer;
 }
