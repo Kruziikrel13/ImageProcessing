5f518329b916148e2f84bceddf813f4dd25e7cdf
kruziikrel13 <dev@michaelpetersen.io>
2025-10-25 21:04:24 +1000

    fix: small fixes to shared header file
    - added pragma once
    - removed explicit arg name from getDirFileNames function signature

--- include/shared.h
+++ include/shared.h
@@ -1,5 +1,6 @@
+#pragma once
 
-int getDirFileNames(const std::string dir, std::vector<std::string>& files);
+int getDirFileNames(const std::string, std::vector<std::string>&);
 
 // Primarily for loading shaders, however does arbitrarily load files
 std::vector<char> readFile(const std::string&);
